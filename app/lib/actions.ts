12a479617be9a1e862f6a098f8bcb8c7e1bd0cf6
jamesphillipdeguzman <jamesphillipdeguzman@gmail.com>
2025-11-12 22:05:26 +0800

    Handling Errors

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -8,35 +8,44 @@ import postgres from 'postgres';
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 const FormSchema = z.object({
-    id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
-    date: z.string(),
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
 });
 // Use Zod to update the expected types
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
-    const { customerId, amount, status } = CreateInvoice.parse({
-        customerId: formData.get('customerId'),
-        amount: formData.get('amount'),
-        status: formData.get('status'),
-    });
-    const amountInCents = amount * 100;
-    const date = new Date().toISOString().split('T')[0];
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
 
+  try {
     await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
 
-    revalidatePath('/dashboard/invoices');
-    redirect('/dashboard/invoices');
-    // Test it out:
-    //   console.log(CreateInvoice);
-    //   console.log(CreateInvoice.shape.amount);
+  } catch (error) {
+    console.error('Error inserting invoice:', error);
+    return {
+      message: 'Datbase error: Failed to create invoice.',
+    };
+  }
+
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+  // Test it out:
+  //   console.log(CreateInvoice);
+  //   console.log(CreateInvoice.shape.amount);
 }
 
 export async function updateInvoice(id: string, formData: FormData) {
@@ -45,20 +54,29 @@ export async function updateInvoice(id: string, formData: FormData) {
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
- 
+
   const amountInCents = amount * 100;
- 
-  await sql`
+
+  try {
+    await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
- 
+
+  } catch (error) {
+    console.error('Error updating invoice:', error);
+    return {
+      message: 'Datbase error: Failed to update invoice.',
+    };
+  }
+
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
+  // throw new Error('Failed to Delete Invoice');
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath('/dashboard/invoices');
 }
